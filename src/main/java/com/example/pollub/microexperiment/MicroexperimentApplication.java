664652c507c5937dbd7f7152621c99a7effc0494
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2023-11-14 18:11:05 +0100

    init spring boot test service with dockerfile

---
+++ src/main/java/com/example/pollub/microexperiment/MicroexperimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.pollub.microexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroexperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroexperimentApplication.class, args);
+	}
+
+}
